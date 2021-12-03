6091d996156e9f52a38b633a857f5fbd1a5b2f49
GitHub Action <action@github.com>
2021-12-03 08:18:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/barfurth/plasticjbide/services/MyProjectService.kt
+++ src/main/kotlin/com/github/barfurth/plasticjbide/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.barfurth.plasticjbide.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.barfurth.plasticjbide.MyBundle
 
 class MyProjectService(project: Project) {
 
